407de4ea121111d467b0c44c960b1123792014e9
Lilian Gallon <lilian.gallon@4sh.fr>
2024-02-28 18:45:09 +0100

    front(global): add toastr

--- dasbod-front/src/app/app.module.ts
+++ dasbod-front/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import {NgModule} from "@angular/core";
 import {HttpClientModule} from "@angular/common/http";
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
-  imports: [HttpClientModule],
+  imports: [
+    HttpClientModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
   providers: [HttpClientModule]
 })
 export class AppModule {

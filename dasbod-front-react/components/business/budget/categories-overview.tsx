80fc798b466bf7b41d127dc4ca019eb4154cdfb0
Lilian Gallon <lilian.gallon@4sh.fr>
2024-03-11 23:27:04 +0100

    front(budget): started working on categories-overview.tsx

---
+++ dasbod-front-react/components/business/budget/categories-overview.tsx
@@ -0,0 +1,52 @@
+import {Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts"
+import React from "react";
+
+const data = [
+    {
+        name: "Jan",
+        spent: 500,
+        remaining: 100
+    },
+    {
+        name: "Feb",
+        spent: 38,
+        remaining: 468
+    }
+]
+
+export function CategoriesOverview() {
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={data}
+                      width={500}
+                      height={300}
+                      margin={{
+                          top: 20,
+                          right: 30,
+                          left: 20,
+                          bottom: 5,
+                      }}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip />
+                <Legend></Legend>
+                <Bar
+                    dataKey="spent"
+                    // fill="currentColor"
+                    // radius={[4, 4, 0, 0]}
+                    // className="fill-primary"
+                    fill="#82ca9d"
+                    stackId="a"
+                />
+                <Bar
+                    dataKey="remaining"
+                    // fill="currentColor"
+                    // radius={[4, 4, 0, 0]}
+                    // className="fill-primary-foreground"
+                    fill="#8884d8"
+                    stackId="a"
+                />
+            </BarChart>
+        </ResponsiveContainer>
+    )
+}

9171019e51701b75e190d91d311f3bfbfee178d2
Lilian Gallon <lilian.gallon@4sh.fr>
2024-04-01 20:24:53 +0200

    front(budget) #4: create budget plan contents

---
+++ dasbod-front/components/business/budget/content/budget-plan-select-content.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { BudgetPlanDialog } from "@/components/business/budget/dialog/budget-plan-dialog"
+
+export function BudgetPlanSelectContent() {
+  return (
+    <div>
+      <BudgetPlanDialog></BudgetPlanDialog>
+    </div>
+  )
+}

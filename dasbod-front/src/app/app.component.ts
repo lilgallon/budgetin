99ff554b27b4a609c8d7c9ab9c02d2d2c12b78c2
lilgallon <lilian@gallon.dev>
2025-01-09 22:14:31 +0100

    front(bootstrap) #7: create menubar

--- dasbod-front/src/app/app.component.ts
+++ dasbod-front/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {PrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { MenubarComponent } from "./shared/components/menubar/menubar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- dasbod-front/src/app/shared/components/button/button.component.ts
+++ dasbod-front/src/app/shared/components/button/button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'ds-button',
+  selector: 'app-button',
   imports: [ButtonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'

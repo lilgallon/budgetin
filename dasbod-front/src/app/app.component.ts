75cec185dd953d94678272bd129a05f785b928e0
lilgallon <lilian@gallon.dev>
2025-01-09 20:09:26 +0100

    front(bootstrap) #7: add tailwind support for primeNG

--- dasbod-front/src/app/app.component.ts
+++ dasbod-front/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonComponent} from './shared/components/button/button.component';
+import {PrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dasbod-front';
+
+  constructor(private primeng: PrimeNG) {
+    this.primeng.theme.set({
+      preset: Aura,
+      options: {
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities'
+        }
+      }
+    })
+  }
 }

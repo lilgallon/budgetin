lilgallon/dasbod

front(budget) #4: WIP budget module

dasbod-front/src/app/budgetin/models/budget-entities.ts
@@ -1,0 +1,24 @@
+import { EntityData } from '../../models/common';
+
+export interface BudgetPlan extends EntityData {
+  amountAtStart: number;
+  expectedIncome: number;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface BudgetCategory extends EntityData {
+  name: string;
+  amount: number;
+  budgetPlanId: string;
+}
+
+export interface BudgetTransaction extends EntityData {
+  date: string;
+  categoryId: string;
+  amount: number;
+  description: string;
+  status: BudgetTransactionStatus;
+}
+
+export type BudgetTransactionStatus = 'PAID' | 'PROCESSING' | 'PLANNED';

lilgallon/budgetin

dasbod-front/src/app/budgetin/models/budget-entities.ts
@@ -1,0 +1,26 @@
+import { EntityData } from '../../models/common';
+
+export interface BudgetPlan extends EntityData {
+  amountAtStart: number;
+  expectedIncome: number;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface BudgetCategory extends EntityData {
+  name: string;
+  amount: number;
+  budgetPlanId: string;
+}
+
+export interface BudgetTransaction extends EntityData {
+  date: Date;
+  categoryId: string;
+  amount: number;
+  description: string;
+  status: BudgetTransactionStatus;
+}
+
+export type BudgetTransactionStatus = 'PAID' | 'PROCESSING' | 'PLANNED';
+
+export const BudgetTransactionStatuses = ['PAID', 'PROCESSING', 'PLANNED'];